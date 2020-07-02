845e27646195af55dceb6b23c45c345be974370b
HyeonJooo <parkhj2416@gmail.com>
2020-07-02 21:34:52 +0900

    post index

---
+++ mongle/routes/post/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+router.use('/theme',require('./theme'));
+router.use('/sentence',require('./sentence'));
+
+//router.put('/:themeIdx', )
+
+module.exports = router;
