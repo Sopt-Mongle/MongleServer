8a25723d3be7809e2545584a0059a6eb954af1ec
HyeonJooo <parkhj2416@gmail.com>
2020-07-06 15:37:28 +0900

    rename save to bookmark

--- mongle/routes/detail/index.js
+++ mongle/routes/detail/index.js
@@ -4,10 +4,10 @@ const detailController = require('../../controllers/detail');
 
 //router.get('/:themeIdx', )
 router.put('/:themeIdx/themelike', detailController.likeTheme);
-router.put('/:themeIdx/themesave', detailController.saveTheme);
+router.put('/:themeIdx/themebookmark', detailController.bookmarkTheme);
 router.get('/bookSearch', detailController.bookSearch);
 router.get('/:sentenceIdx', detailController.getSentence);
 router.put('/:sentenceIdx/sentencelike', detailController.likeSentence);
-router.put('/:sentenceIdx/sentencesave', detailController.saveSentence);
+router.put('/:sentenceIdx/sentencebookmark', detailController.bookmarkSentence);
 
 module.exports = router;
