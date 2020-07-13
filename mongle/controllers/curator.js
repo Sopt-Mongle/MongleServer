9622a382d9af842c211953e5c2e30b24b6ed32f0
khl6235 <khl6235@naver.com>
2020-07-14 01:04:28 +0900

    [테마 속 큐레이터 목록 조회] 수정

--- mongle/controllers/curator.js
+++ mongle/controllers/curator.js
@@ -5,7 +5,7 @@ const resMessage = require('../modules/responseMessage');
 const CuratorModel = require('../models/curator');
 
 module.exports = {
-    getAllCurators : async(req, res) => {
+    /*getAllCurators : async(req, res) => {
         const curatorIdx = req.body.curatorIdx;
         if(!curatorIdx){
             res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
@@ -20,9 +20,9 @@ module.exports = {
         }
 
         return res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.CURATER_SHOW_ALL, result));
-    },
+    },*/
 
-    subscribe : async(req, res) => {
+    subscribe : async(req, res) => { //고쳐야함!!!!!!!
         const curatorIdx = req.params.curatorIdx; //현재 사용자 큐레이터
         const followedIdx = req.body.followedIdx; //구독 or 구독취소 할 큐레이터
 
--- mongle/routes/curator/index.js
+++ mongle/routes/curator/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var CuratorController = require('../../controllers/curator');
 
-router.get('/', CuratorController.getAllCurators);
+// router.get('/', CuratorController.getAllCurators);
 router.put('/:curatorIdx', CuratorController.subscribe);
 router.get('/recommend', CuratorController.getRecommendCurator);
 router.get('/themeInCurator', CuratorController.getThemeInCurator);
