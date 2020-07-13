084e54f279ee5ea1036e24898b36b9bb5485d557
khl6235 <khl6235@naver.com>
2020-07-14 03:17:15 +0900

    코드 정리

--- mongle/controllers/detail.js
+++ mongle/controllers/detail.js
@@ -36,8 +36,6 @@ module.exports = {
         function Func1(){
             const isLike = detailModel.isLike(curatorIdx, sentenceIdx);
             return isLike;
-            //isLike.then((result) => console.log(result));
-            // console.log(isLike);
         };
 
         let data;
@@ -49,10 +47,10 @@ module.exports = {
             else{//좋아요 추가
                 data = await detailModel.addLike(curatorIdx, sentenceIdx);
             }
-            // console.log(isLike, data[0]);
+            
             result.isLike = isLike;
             result.likes = data[0].likes;
-            // console.log(result);
+            
             return result;
         };
 
@@ -60,9 +58,6 @@ module.exports = {
             console.log(elem);
         }).then((res) => Func2(res));
 
-        // console.log(tmp);
-
-        // console.log(result);
         return await res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.LIKE_SENTENCE, result));
     },
 
@@ -132,44 +127,6 @@ module.exports = {
         const result = await detailModel.getTheme(curatorIdx, themeIdx);
         return await res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.READ_THEME, result));
     },
-
-    /*
-    likeTheme : async(req, res) =>{
-        const themeIdx = req.params.themeIdx;
-        if(!themeIdx){
-            return await res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE_THEME));
-        }
-        const {curatorIdx} = req.body;
-        if(!curatorIdx){
-            return await res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE_CURATOR));
-        }
-
-        function Func1(){
-            const isLike = detailModel.themeIsLike(curatorIdx, themeIdx);
-            return isLike;
-        };
-
-        let data;
-        var result = {};
-        async function Func2(isLike){
-            if(!isLike){//이미 좋아요이니까 취소
-                data = await detailModel.themeDeleteLike(curatorIdx, themeIdx);
-            }
-            else{//좋아요 추가
-                data = await detailModel.themeAddLike(curatorIdx, themeIdx);
-            }
-            result.isLike = isLike;
-            result.likes = data[0].likes;
-            return result;
-        };
-
-        await Func1(async(elem) =>{
-            console.log(elem);
-        }).then((res) => Func2(res));
-
-        return await res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.LIKE_THEME, result));
-    },
-    */
 
     bookmarkTheme : async(req, res) =>{
         const themeIdx = req.params.themeIdx;
