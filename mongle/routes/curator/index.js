6dbd25ec0ecfa477211a87c9d66948ec6f7b6f93
khl6235 <khl6235@naver.com>
2020-11-06 23:08:24 +0900

    비회원 사용가능 - 토큰 제거

--- mongle/routes/curator/index.js
+++ mongle/routes/curator/index.js
@@ -4,10 +4,10 @@ var CuratorController = require('../../controllers/curator');
 
 const authUtil = require('../../modules/authUtil');
 
-router.get('/themeInCurator', authUtil.checkToken, CuratorController.getThemeInCurator);
+router.get('/themeInCurator', CuratorController.getThemeInCurator);
 router.get('/recommend', CuratorController.getRecommendCurator);
-router.get('/:curatorIdx', authUtil.checkToken, CuratorController.getCuratorInfo);
+router.get('/:curatorIdx', CuratorController.getCuratorInfo);
 router.put('/:followedIdx', authUtil.checkToken, CuratorController.subscribe);
-router.get('/:keywordIdx/keyword', authUtil.checkToken, CuratorController.getCuratorByKeyword);
+router.get('/:keywordIdx/keyword', CuratorController.getCuratorByKeyword);
 
 module.exports = router;
