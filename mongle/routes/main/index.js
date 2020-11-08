87421220f9b080442cb1106e22fbb317dd7b814f
khl6235 <khl6235@naver.com>
2020-11-09 00:52:59 +0900

    main쪽 토큰 빼기

--- mongle/routes/main/index.js
+++ mongle/routes/main/index.js
@@ -4,10 +4,10 @@ const mainController = require('../../controllers/main');
 const authUtil = require('../../modules/authUtil');
 
 router.get('/editorsPick', mainController.editorsPick);
-router.get('/sentences', authUtil.checkToken, mainController.getTodaySentence);
-router.get('/themes', authUtil.checkToken, mainController.getTodayTheme);
+router.get('/sentences', mainController.getTodaySentence);
+router.get('/themes', mainController.getTodayTheme);
 router.get('/curators', mainController.getTodayCurator);
-router.get('/waitThemes', authUtil.checkToken, mainController.getWaitTheme);
-router.get('/nowThemes', authUtil.checkToken, mainController.getNowTheme);
+router.get('/waitThemes', mainController.getWaitTheme);
+router.get('/nowThemes', mainController.getNowTheme);
 
 module.exports = router;
